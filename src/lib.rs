fb9a9c0e56dc1c7808ffc5d127d78e99ea23b88e
Scraft161 <scraft161@gmail.com>
2024-02-03 22:15:28 +0100

    use hard tabs & cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -5,219 +5,219 @@ use std::path::PathBuf;
 use std::str::FromStr;
 
 pub trait FTLTrait {
-    fn read(_: Option<DirEntry>) -> Self;
+	fn read(_: Option<DirEntry>) -> Self;
 
-    fn write(_: Option<DirEntry>);
+	fn write(_: Option<DirEntry>);
 
-    fn from_string(_: String) -> Self;
+	fn from_string(_: String) -> Self;
 }
 
 pub type FTagList = Vec<FTagFile>;
 
 impl FTLTrait for FTagList {
-    /// Read ftags from a file
-    ///
-    /// `panic!()`s when file is `None`
-    fn read(file: Option<DirEntry>) -> Self {
-        if file.is_none() {
-            panic!()
-        }
+	/// Read ftags from a file
+	///
+	/// `panic!()`s when file is `None`
+	fn read(file: Option<DirEntry>) -> Self {
+		if file.is_none() {
+			panic!()
+		}
 
-        let contents = fs::read_to_string(file.unwrap().file_name()).unwrap();
-        Self::from_string(contents)
-    }
+		let contents = fs::read_to_string(file.unwrap().file_name()).unwrap();
+		Self::from_string(contents)
+	}
 
-    fn write(file: Option<DirEntry>) {
-        todo!()
-    }
+	fn write(file: Option<DirEntry>) {
+		todo!()
+	}
 
-    fn from_string(string: String) -> Self {
-        let mut ftags = Vec::new();
+	fn from_string(string: String) -> Self {
+		let mut ftags = Vec::new();
 
-        for tagged_file in string.split('\n') {
-            if tagged_file.is_empty() {
-                continue;
-            }
+		for tagged_file in string.split('\n') {
+			if tagged_file.is_empty() {
+				continue;
+			}
 
-            ftags.push(FTagFile::from_str(tagged_file).unwrap());
-        }
+			ftags.push(FTagFile::from_str(tagged_file).unwrap());
+		}
 
-        ftags
-    }
+		ftags
+	}
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 pub struct FTagFile {
-    pub file: PathBuf,
-    pub tags: Vec<FTag>,
+	pub file: PathBuf,
+	pub tags: Vec<FTag>,
 }
 
 impl FromStr for FTagFile {
-    type Err = String;
+	type Err = String;
 
-    fn from_str(str: &str) -> Result<Self, Self::Err> {
-        if let Some((file, tags_str)) = str.split_once(':') {
-            let mut tags = Vec::new();
-            tags_str
-                .split(',')
-                .for_each(|s| tags.push(FTag::from_str(s.trim()).unwrap()));
-            Ok(Self {
-                file: PathBuf::from_str(file).unwrap(),
-                tags,
-            })
-        } else {
-            Err("ɑ".to_string())
-        }
-    }
+	fn from_str(str: &str) -> Result<Self, Self::Err> {
+		if let Some((file, tags_str)) = str.split_once(':') {
+			let mut tags = Vec::new();
+			tags_str
+				.split(',')
+				.for_each(|s| tags.push(FTag::from_str(s.trim()).unwrap()));
+			Ok(Self {
+				file: PathBuf::from_str(file).unwrap(),
+				tags,
+			})
+		} else {
+			Err("ɑ".to_string())
+		}
+	}
 }
 impl fmt::Display for FTagFile {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        if self.tags.is_empty() {
-            return Ok(());
-        }
+	fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+		if self.tags.is_empty() {
+			return Ok(());
+		}
 
-        let mut tags_str = String::new();
+		let mut tags_str = String::new();
 
-        for (i, tag) in self.tags.iter().enumerate() {
-            if i != self.tags.len() && i != 0 {
-                tags_str.push_str(", ");
-            }
+		for (i, tag) in self.tags.iter().enumerate() {
+			if i != self.tags.len() && i != 0 {
+				tags_str.push_str(", ");
+			}
 
-            tags_str.push_str(&format!("{}", tag));
-        }
+			tags_str.push_str(&format!("{}", tag));
+		}
 
-        write!(f, "{}: {}", self.file.display(), tags_str)
-    }
+		write!(f, "{}: {}", self.file.display(), tags_str)
+	}
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 pub struct FTag {
-    pub name: String,
-    pub data: Option<FTagData>,
+	pub name: String,
+	pub data: Option<FTagData>,
 }
 
 impl FromStr for FTag {
-    type Err = String;
+	type Err = String;
 
-    fn from_str(str: &str) -> Result<Self, Self::Err> {
-        if !str.contains(':') {
-            Ok(Self {
-                name: str.to_string(),
-                data: None,
-            })
-        } else {
-            if let Some((name, tag_data)) = str.split_once(':') {
-                let data = FTagData::from_str(tag_data);
+	fn from_str(str: &str) -> Result<Self, Self::Err> {
+		if !str.contains(':') {
+			Ok(Self {
+				name: str.to_string(),
+				data: None,
+			})
+		} else {
+			if let Some((name, tag_data)) = str.split_once(':') {
+				let data = FTagData::from_str(tag_data);
 
-                Ok(Self {
-                    name: name.to_string(),
-                    data: Some(data?),
-                })
-            } else {
-                Err(format!("Could not parse `{}` into a valid tag.", str))
-            }
-        }
-    }
+				Ok(Self {
+					name: name.to_string(),
+					data: Some(data?),
+				})
+			} else {
+				Err(format!("Could not parse `{}` into a valid tag.", str))
+			}
+		}
+	}
 }
 
 impl fmt::Display for FTag {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        if let Some(data) = &self.data {
-            match data {
-                FTagData::Single(tag_data) => write!(f, "{}:{}", self.name, tag_data),
-                FTagData::List(tag_list) => {
-                    let mut data_str = String::new();
+	fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+		if let Some(data) = &self.data {
+			match data {
+				FTagData::Single(tag_data) => write!(f, "{}:{}", self.name, tag_data),
+				FTagData::List(tag_list) => {
+					let mut data_str = String::new();
 
-                    for (i, list_item) in tag_list.iter().enumerate() {
-                        if i != tag_list.len() && i != 0 {
-                            data_str.push(' ');
-                        }
+					for (i, list_item) in tag_list.iter().enumerate() {
+						if i != tag_list.len() && i != 0 {
+							data_str.push(' ');
+						}
 
-                        data_str.push_str(list_item);
-                    }
+						data_str.push_str(list_item);
+					}
 
-                    write!(f, "{}:[{}]", self.name, data_str)
-                }
-            }
-        } else {
-            write!(f, "{}", self.name)
-        }
-    }
+					write!(f, "{}:[{}]", self.name, data_str)
+				}
+			}
+		} else {
+			write!(f, "{}", self.name)
+		}
+	}
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
 pub enum FTagData {
-    Single(String),
-    List(Vec<String>),
+	Single(String),
+	List(Vec<String>),
 }
 
 impl FromStr for FTagData {
-    type Err = String;
+	type Err = String;
 
-    fn from_str(str: &str) -> Result<Self, Self::Err> {
-        if str.starts_with('[') && str.ends_with(']') {
-            let mut data_list = Vec::new();
-            str.split(' ').for_each(|s| {
-                // TODO: clean this mess TwT
-                if s.starts_with('[') && s.ends_with(']') {
-                    data_list.push(s[1..s.len() - 1].trim().to_string())
-                } else if s.starts_with('[') {
-                    data_list.push(s[1..].trim().to_string())
-                } else if s.ends_with(']') {
-                    data_list.push(s[0..s.len() - 1].trim().to_string())
-                } else {
-                    data_list.push(s.trim().to_string())
-                }
-            });
-            Ok(FTagData::List(data_list))
-        } else {
-            Ok(FTagData::Single(str.to_string()))
-        }
-    }
+	fn from_str(str: &str) -> Result<Self, Self::Err> {
+		if str.starts_with('[') && str.ends_with(']') {
+			let mut data_list = Vec::new();
+			str.split(' ').for_each(|s| {
+				// TODO: clean this mess TwT
+				if s.starts_with('[') && s.ends_with(']') {
+					data_list.push(s[1..s.len() - 1].trim().to_string())
+				} else if s.starts_with('[') {
+					data_list.push(s[1..].trim().to_string())
+				} else if s.ends_with(']') {
+					data_list.push(s[0..s.len() - 1].trim().to_string())
+				} else {
+					data_list.push(s.trim().to_string())
+				}
+			});
+			Ok(FTagData::List(data_list))
+		} else {
+			Ok(FTagData::Single(str.to_string()))
+		}
+	}
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_parse_ftags() {
-        let ftags_str = "a: tag, tag2
+	#[test]
+	fn test_parse_ftags() {
+		let ftags_str = "a: tag, tag2
 foo/b.jpg: file_type:jpg, img_tags:[1girl solo standing long_hair]";
-        let ftags_proper: FTagList = vec![
-            FTagFile {
-                file: String::from("a").into(),
-                tags: vec![
-                    FTag {
-                        name: String::from("tag"),
-                        data: None,
-                    },
-                    FTag {
-                        name: String::from("tag2"),
-                        data: None,
-                    },
-                ],
-            },
-            FTagFile {
-                file: String::from("foo/b.jpg").into(),
-                tags: vec![
-                    FTag {
-                        name: String::from("file_type"),
-                        data: Some(FTagData::Single(String::from("jpg"))),
-                    },
-                    FTag {
-                        name: String::from("img_tags"),
-                        data: Some(FTagData::List(vec![
-                            String::from("1girl"),
-                            String::from("solo"),
-                            String::from("standing"),
-                            String::from("long_hair"),
-                        ])),
-                    },
-                ],
-            },
-        ];
+		let ftags_proper: FTagList = vec![
+			FTagFile {
+				file: String::from("a").into(),
+				tags: vec![
+					FTag {
+						name: String::from("tag"),
+						data: None,
+					},
+					FTag {
+						name: String::from("tag2"),
+						data: None,
+					},
+				],
+			},
+			FTagFile {
+				file: String::from("foo/b.jpg").into(),
+				tags: vec![
+					FTag {
+						name: String::from("file_type"),
+						data: Some(FTagData::Single(String::from("jpg"))),
+					},
+					FTag {
+						name: String::from("img_tags"),
+						data: Some(FTagData::List(vec![
+							String::from("1girl"),
+							String::from("solo"),
+							String::from("standing"),
+							String::from("long_hair"),
+						])),
+					},
+				],
+			},
+		];
 
-        assert_eq!(FTagList::from_string(ftags_str.to_string()), ftags_proper);
-    }
+		assert_eq!(FTagList::from_string(ftags_str.to_string()), ftags_proper);
+	}
 }
--- src/main.rs
+++ src/main.rs
@@ -8,177 +8,177 @@ use ftags::{FTLTrait, FTag, FTagList};
 #[command(name = "ftags")]
 #[command(bin_name = "ftags")]
 enum Commands {
-    /// List all tags for a given file          (shorthands: `l`, `ls`)
-    #[clap(aliases = &["l", "ls"])]
-    List { file: std::path::PathBuf },
-    /// List all tags                           (shorthands: `t`, `lt`)
-    #[clap(aliases = &["t", "lt"])]
-    ListTags,
-    /// Add a tag to a file (Not working)       (shorthands: `a`, `n`)
-    #[clap(aliases = &["a", "n"])]
-    Add {
-        file: std::path::PathBuf,
-        tags: Vec<FTag>,
-    },
-    /// Remove a tag from a file (not working)  (shorthands: `r`, `d`)
-    #[clap(aliases = &["r", "d"])]
-    Remove {
-        file: std::path::PathBuf,
-        tags: Vec<FTag>,
-    },
-    /// Search files for a given tag            (shorthands: `s`, `f`)
-    #[clap(aliases = &["s", "f"])]
-    Search { tags: Vec<FTag> },
+	/// List all tags for a given file          (shorthands: `l`, `ls`)
+	#[clap(aliases = &["l", "ls"])]
+	List { file: std::path::PathBuf },
+	/// List all tags                           (shorthands: `t`, `lt`)
+	#[clap(aliases = &["t", "lt"])]
+	ListTags,
+	/// Add a tag to a file (Not working)       (shorthands: `a`, `n`)
+	#[clap(aliases = &["a", "n"])]
+	Add {
+		file: std::path::PathBuf,
+		tags: Vec<FTag>,
+	},
+	/// Remove a tag from a file (not working)  (shorthands: `r`, `d`)
+	#[clap(aliases = &["r", "d"])]
+	Remove {
+		file: std::path::PathBuf,
+		tags: Vec<FTag>,
+	},
+	/// Search files for a given tag            (shorthands: `s`, `f`)
+	#[clap(aliases = &["s", "f"])]
+	Search { tags: Vec<FTag> },
 }
 
 #[derive(Parser)]
 struct Args {
-    /// Print script-friendly output.
-    #[arg(long, short)]
-    script: bool,
+	/// Print script-friendly output.
+	#[arg(long, short)]
+	script: bool,
 
-    #[clap(subcommand)]
-    cmd: Commands,
+	#[clap(subcommand)]
+	cmd: Commands,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Args::parse();
-    let command = args.cmd;
-    let ftags_file = match std::path::Path::new(".").read_dir().unwrap().find(|f| {
-        //dbg!(f);
-        f.as_ref().unwrap().file_name() == ".ftags"
-    }) {
-        Some(file) => {
-            if let Ok(file) = file {
-                Some(file)
-            } else {
-                None
-            }
-        }
-        None => None,
-    };
+	let args = Args::parse();
+	let command = args.cmd;
+	let ftags_file = match std::path::Path::new(".").read_dir().unwrap().find(|f| {
+		//dbg!(f);
+		f.as_ref().unwrap().file_name() == ".ftags"
+	}) {
+		Some(file) => {
+			if let Ok(file) = file {
+				Some(file)
+			} else {
+				None
+			}
+		}
+		None => None,
+	};
 
-    let tag_delimiter = match args.script {
-        true => "\n",
-        false => ", ",
-    };
+	let tag_delimiter = match args.script {
+		true => "\n",
+		false => ", ",
+	};
 
-    match command {
-        Commands::List { file } => {
-            if ftags_file.is_none() {
-                return Err("No `.ftags` file found!".into());
-            }
+	match command {
+		Commands::List { file } => {
+			if ftags_file.is_none() {
+				return Err("No `.ftags` file found!".into());
+			}
 
-            let ftags = FTagList::read(ftags_file);
-            let mut ftags_for_file = None;
+			let ftags = FTagList::read(ftags_file);
+			let mut ftags_for_file = None;
 
-            for tag in ftags {
-                if tag.file == file {
-                    ftags_for_file = Some(tag);
-                    break;
-                }
-            }
-            if let Some(file) = ftags_for_file {
-                // Print tags
-                println!("{}", file)
-            } else {
-                return Err(format!("No tags for `{}`.", file.display()).into());
-            }
-        }
-        Commands::ListTags => {
-            if ftags_file.is_none() {
-                return Err("No `.ftags` file found!".into());
-            }
+			for tag in ftags {
+				if tag.file == file {
+					ftags_for_file = Some(tag);
+					break;
+				}
+			}
+			if let Some(file) = ftags_for_file {
+				// Print tags
+				println!("{}", file)
+			} else {
+				return Err(format!("No tags for `{}`.", file.display()).into());
+			}
+		}
+		Commands::ListTags => {
+			if ftags_file.is_none() {
+				return Err("No `.ftags` file found!".into());
+			}
 
-            let ftags = FTagList::read(ftags_file);
+			let ftags = FTagList::read(ftags_file);
 
-            let mut tags_list = Vec::new();
+			let mut tags_list = Vec::new();
 
-            for tag_file in ftags {
-                for tag in tag_file.tags {
-                    tags_list.push(tag);
-                }
-            }
+			for tag_file in ftags {
+				for tag in tag_file.tags {
+					tags_list.push(tag);
+				}
+			}
 
-            tags_list.sort_by(|a, b| match a.name.cmp(&b.name) {
-                Ordering::Equal => a.data.cmp(&b.data),
-                other => other,
-            });
-            tags_list.dedup();
+			tags_list.sort_by(|a, b| match a.name.cmp(&b.name) {
+				Ordering::Equal => a.data.cmp(&b.data),
+				other => other,
+			});
+			tags_list.dedup();
 
-            // Print the list
-            println!("{}", join_vec(tags_list, tag_delimiter));
-        }
-        Commands::Add { file, tags } => {
-            dbg!(&file, &tags);
-            let ftags = FTagList::read(ftags_file);
-            dbg!(&ftags);
-        }
-        Commands::Remove { file, tags } => {
-            todo!();
-        }
-        Commands::Search { tags } => {
-            let ftags = FTagList::read(ftags_file);
-            let mut partial_matches = Vec::new();
-            let mut full_matches = Vec::new();
+			// Print the list
+			println!("{}", join_vec(tags_list, tag_delimiter));
+		}
+		Commands::Add { file, tags } => {
+			dbg!(&file, &tags);
+			let ftags = FTagList::read(ftags_file);
+			dbg!(&ftags);
+		}
+		Commands::Remove { file, tags } => {
+			todo!();
+		}
+		Commands::Search { tags } => {
+			let ftags = FTagList::read(ftags_file);
+			let mut partial_matches = Vec::new();
+			let mut full_matches = Vec::new();
 
-            for tag_file in ftags {
-                let mut full_match = false;
-                let mut partial_match = false;
+			for tag_file in ftags {
+				let mut full_match = false;
+				let mut partial_match = false;
 
-                for file_tag in &tag_file.tags {
-                    for search_tag in &tags {
-                        // Name match
-                        if file_tag.name == search_tag.name {
-                            partial_match = true;
+				for file_tag in &tag_file.tags {
+					for search_tag in &tags {
+						// Name match
+						if file_tag.name == search_tag.name {
+							partial_match = true;
 
-                            // Only match if the user provided tag data to match
-                            if let Some(search_tag_data) = &search_tag.data {
-                                if let Some(file_tag_data) = &file_tag.data {
-                                    if search_tag_data == file_tag_data {
-                                        full_match = true;
-                                    }
-                                }
-                            } else {
-                                full_match = true;
-                            }
-                        }
-                    }
-                }
+							// Only match if the user provided tag data to match
+							if let Some(search_tag_data) = &search_tag.data {
+								if let Some(file_tag_data) = &file_tag.data {
+									if search_tag_data == file_tag_data {
+										full_match = true;
+									}
+								}
+							} else {
+								full_match = true;
+							}
+						}
+					}
+				}
 
-                if partial_match {
-                    partial_matches.push(tag_file.clone());
-                }
+				if partial_match {
+					partial_matches.push(tag_file.clone());
+				}
 
-                if full_match {
-                    full_matches.push(tag_file.clone());
-                }
-            }
+				if full_match {
+					full_matches.push(tag_file.clone());
+				}
+			}
 
-            partial_matches.sort();
-            full_matches.sort();
+			partial_matches.sort();
+			full_matches.sort();
 
-            // Print results to the screen
-            if !full_matches.is_empty() {
-                let matches_pretty = join_vec(full_matches.clone(), "\n    ");
-                println!("Full matches:\n    {matches_pretty}");
-            }
-            if !partial_matches.is_empty() {
-                let matches_pretty = join_vec(partial_matches, "\n    ");
-                if !full_matches.is_empty() {
-                    let full_matches_pretty = join_vec(full_matches.clone(), "\n    ");
-                    if full_matches_pretty == matches_pretty {
-                        return Ok(());
-                    }
-                    println!();
-                }
+			// Print results to the screen
+			if !full_matches.is_empty() {
+				let matches_pretty = join_vec(full_matches.clone(), "\n    ");
+				println!("Full matches:\n    {matches_pretty}");
+			}
+			if !partial_matches.is_empty() {
+				let matches_pretty = join_vec(partial_matches, "\n    ");
+				if !full_matches.is_empty() {
+					let full_matches_pretty = join_vec(full_matches.clone(), "\n    ");
+					if full_matches_pretty == matches_pretty {
+						return Ok(());
+					}
+					println!();
+				}
 
-                println!("Partial Matches:\n    {matches_pretty}");
-            }
-        }
-    }
+				println!("Partial Matches:\n    {matches_pretty}");
+			}
+		}
+	}
 
-    Ok(())
+	Ok(())
 }
 
 //struct TagCount {
@@ -187,27 +187,27 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 //}
 
 fn join_vec<T: ToString>(vec: Vec<T>, delimiter: &str) -> String {
-    let mut joined = String::new();
+	let mut joined = String::new();
 
-    for (i, item) in vec.iter().enumerate() {
-        let item_string = item.to_string();
-        joined.push_str(&item_string);
+	for (i, item) in vec.iter().enumerate() {
+		let item_string = item.to_string();
+		joined.push_str(&item_string);
 
-        if i != vec.len() - 1 {
-            joined.push_str(delimiter);
-        }
-    }
+		if i != vec.len() - 1 {
+			joined.push_str(delimiter);
+		}
+	}
 
-    joined
+	joined
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn test_join_vec() {
-        let elements = vec!["a", "b", "c"];
-        assert_eq!(join_vec(elements, ", "), "a, b, c");
-    }
+	#[test]
+	fn test_join_vec() {
+		let elements = vec!["a", "b", "c"];
+		assert_eq!(join_vec(elements, ", "), "a, b, c");
+	}
 }
